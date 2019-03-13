b9167b8e618489ebd1ce38b4d9852d79edd914fc
hschneider <harry.schneider@aquilaheywood.co.uk>
2019-03-13 08:29:55 +0000

    Added unit tests

--- bubble_sort.erl
+++ bubble_sort.erl
@@ -1,5 +1,6 @@
 -module(bubble_sort).
 -export([sort/1]).
+-include_lib("eunit/include/eunit.hrl").
 
 sort([H|T]) ->
 	sort(H,T,[],[]).
@@ -21,3 +22,7 @@ reverse([],List) ->
 	List;
 reverse([H|T],List) ->
 	reverse(T,[H|List]).
+
+sort_random_test() -> [-92,-44,-20,12,21,26,48,51,80,86] = sort([48,12,86,26,-20,21,80,-92,51,-44]).
+sort_sorted_test() -> [-92,-44,-20,12,21,26,48,51,80,86] = sort([-92,-44,-20,12,21,26,48,51,80,86]).
+sort_reverse_test() -> [-92,-44,-20,12,21,26,48,51,80,86] = sort([86,80,51,48,26,21,12,-20,-44,-92]).
