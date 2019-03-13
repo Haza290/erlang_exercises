e08440334f083488bd1f9cd1946edda0b64256b8
hschneider <harry.schneider@aquilaheywood.co.uk>
2019-03-13 08:05:32 +0000

    Fixed pangram as it was trying to compair a string broken down to a list of ints to a single char string

--- pangram.erl
+++ pangram.erl
@@ -1,13 +1,14 @@
 -module(pangram).
--export([is_pangram/1,remove_from_list/2]).
+-export([is_pangram/1]).
+-import(string,[to_lower/1]).
 
 is_pangram(Word) ->
-	Alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"],
-	is_pangram(Word,Alphabet).
+	Alphabet = "abcdefghijklmnopqrstuvwxyz",
+	is_pangram(string:to_lower(Word),Alphabet).
 is_pangram(_,[]) ->
 	true;
-is_pangram([],X) ->
-	{false,X};
+is_pangram([],_) ->
+	false;
 is_pangram([H|T],Alphabet) ->
 	is_pangram(T,remove_from_list(H,Alphabet)).
 
@@ -19,4 +20,3 @@ remove_from_list(X,[X|T],List) ->
 	T++List;
 remove_from_list(X,[H|T],List) ->
 	remove_from_list(X,T,[H|List]).
-
